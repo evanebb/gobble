944393be5a38bb40480851d6cb3adb30709c572b
evanebb <78433178+evanebb@users.noreply.github.com>
2023-02-15 22:12:28 +0100

    Add JSend based response wrapper functions

--- server/response.go
+++ server/response.go
@@ -6,7 +6,40 @@ import (
 	"net/http"
 )
 
-func SendJSONResponse(w http.ResponseWriter, v any) error {
+type response struct {
+	Status  string `json:"status"`
+	Data    any    `json:"data"`
+	Message string `json:"message"`
+}
+
+// SendSuccessResponse sends a JSend-compliant response indicating success, with the passed data nested in it.
+func SendSuccessResponse(w http.ResponseWriter, code int, v any) error {
+	r := response{
+		Status: "success",
+		Data:   v,
+	}
+	return SendJSONResponse(w, code, r)
+}
+
+// SendErrorResponse sends a JSend-compliant response indicating an error with the passed error message.
+func SendErrorResponse(w http.ResponseWriter, code int, message string) error {
+	r := response{
+		Status:  "error",
+		Message: message,
+	}
+	return SendJSONResponse(w, code, r)
+}
+
+// SendFailResponse sends a JSend-compliant response indicating failure with the passed error message.
+func SendFailResponse(w http.ResponseWriter, code int, message string) error {
+	r := response{
+		Status:  "fail",
+		Message: message,
+	}
+	return SendJSONResponse(w, code, r)
+}
+
+func SendJSONResponse(w http.ResponseWriter, code int, v any) error {
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
