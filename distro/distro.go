e601645f648a9eea3d8c1d902f104cb7c226c7cf
evanebb <78433178+evanebb@users.noreply.github.com>
2023-02-15 17:45:07 +0100

    Validate name of distro in constructor

--- distro/distro.go
+++ distro/distro.go
@@ -1,8 +1,10 @@
 package distro
 
 import (
+	"errors"
 	"github.com/evanebb/gobble/kernelparameters"
 	"github.com/google/uuid"
+	"regexp"
 )
 
 type Distro struct {
@@ -14,15 +16,35 @@ type Distro struct {
 	kernelParameters kernelparameters.KernelParameters
 }
 
-func New(id uuid.UUID, name string, description string, kernel string, initrd string, kernelParameters kernelparameters.KernelParameters) Distro {
-	return Distro{
+func New(id uuid.UUID, name string, description string, kernel string, initrd string, kernelParameters kernelparameters.KernelParameters) (*Distro, error) {
+	var d *Distro
+
+	if err := validateName(name); err != nil {
+		return d, err
+	}
+
+	return &Distro{
 		id:               id,
 		name:             name,
 		description:      description,
 		kernel:           kernel,
 		initrd:           initrd,
 		kernelParameters: kernelParameters,
+	}, nil
+}
+
+func validateName(name string) error {
+	p := "^[a-zA-Z0-9-_.()]{1,64}$"
+	matched, err := regexp.MatchString(p, name)
+	if err != nil {
+		return err
 	}
+
+	if !matched {
+		return errors.New("name contains illegal characters")
+	}
+
+	return nil
 }
 
 func (d *Distro) Id() uuid.UUID {
