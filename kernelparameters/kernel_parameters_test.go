ba741f88e6834a4aa2da1652c3d98768acf3ca49
evanebb <78433178+evanebb@users.noreply.github.com>
2023-02-16 22:35:07 +0100

    Add more tests for kernel parameter code

--- kernelparameters/kernel_parameters_test.go
+++ kernelparameters/kernel_parameters_test.go
@@ -1,20 +1,89 @@
 package kernelparameters
 
 import (
+	"errors"
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestNewKernelParameters(t *testing.T) {
-	v := []string{"test1", "test2=value"}
+	v := []string{
+		"test1",
+		"test2=value",
+	}
 
 	expected := KernelParameters{
 		"test1": "",
 		"test2": "value",
 	}
 
-	kp, err := New(v)
-	if !reflect.DeepEqual(kp, expected) || err != nil {
-		t.Fatalf(`New([]string{"test1 test2=value"}) = %v, %v, expected: %v, nil`, kp, err, expected)
+	actual, err := New(v)
+	if !reflect.DeepEqual(actual, expected) || err != nil {
+		t.Fatalf(`New() = %v, %v, expected: %v, nil`, actual, err, expected)
+	}
+}
+
+func TestFormatKernelParameters(t *testing.T) {
+	v := KernelParameters{
+		"test1": "",
+		"test2": "value",
+	}
+
+	expected := []string{
+		"test1",
+		"test2=value",
+	}
+
+	actual := FormatKernelParameters(v)
+
+	// Sort the slices, since the order is not guaranteed and I don't care about it either
+	sort.Strings(actual)
+	sort.Strings(expected)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf(`FormatKernelParameters() = %v, expected: %v`, actual, expected)
+	}
+}
+
+func TestMergeKernelParameters(t *testing.T) {
+	kp1 := KernelParameters{
+		"test1": "",
+		"test2": "value",
+		"test3": "value2",
+	}
+
+	kp2 := KernelParameters{
+		"test2": "newvalue",
+		"test4": "value3",
+	}
+
+	expected := KernelParameters{
+		"test1": "",
+		"test2": "newvalue",
+		"test3": "value2",
+		"test4": "value3",
+	}
+
+	actual := MergeKernelParameters(kp1, kp2)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf(`MergeKernelParameters() = %v, expected: %v`, actual, expected)
+	}
+}
+
+func TestInvalidParameterError(t *testing.T) {
+	var actualErr *InvalidParameterError
+	expectedErr := NewInvalidParameterError("this is invalid")
+
+	v := []string{
+		"this is invalid",
+		"test2=value",
+	}
+
+	expectedValue := make(KernelParameters)
+
+	actualValue, err := New(v)
+	if err == nil || !errors.As(err, &actualErr) {
+		t.Fatalf(`New() = %v, %v, expected: %v, %v`, actualValue, actualErr, expectedValue, expectedErr)
 	}
 }
