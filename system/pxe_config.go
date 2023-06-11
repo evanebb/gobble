9fae0bd2347e968056052dc1dbd083f5c489b836
evanebb <78433178+evanebb@users.noreply.github.com>
2023-06-11 14:42:44 +0200

    Serve iPXE script instead of JSON response if system is not registered

--- system/pxe_config.go
+++ system/pxe_config.go
@@ -8,12 +8,22 @@ import (
 
 // TODO: create a PxeConfig interface with a Render() method, have the System struct implement it
 
+// iPXE script template that is served to clients.
+// The configured kernel, initrd and kernel parameters are substituted into it.
 var template = `#!ipxe
 
 kernel %s %s
 initrd %s
 
 boot
+`
+
+// iPXE script template that is served if the system is not registered and no profile can be found for it.
+var notFoundTemplate = `#!ipxe
+
+echo No matching profile found for system!
+prompt --timeout 30000 Press any key or wait 30 seconds to continue ||
+
 `
 
 type PxeConfig struct {
@@ -34,4 +44,8 @@ func (p PxeConfig) Render() string {
 	kp := kernelparameters.FormatKernelParameters(p.KernelParameters)
 	kpStr := strings.Join(kp, " ")
 	return fmt.Sprintf(template, p.Kernel, kpStr, p.Initrd)
+}
+
+func RenderNotFound() string {
+	return notFoundTemplate
 }
